91d5ae0d28a67c774268ac19a8420d77e2635f0c
isabellagh <isabellagh88@gmail.com>
2021-09-01 22:27:39 -0400

    responsive navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -14,9 +14,9 @@ function Navbar() {
         </div>
         <div className="mobile-menu">
           <div onClick={() => setIsOpen(!isOpen)}>
-            <i className="bi bi-list"></i>
+            <i className="bi bi-list menu-icon"></i>
           </div>
-          <div>{isOpen && <Mobile />}</div>
+          <div>{isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}</div>
         </div>
       </div>
     </div>
