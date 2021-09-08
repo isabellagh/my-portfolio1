4d7475adae15427e3582b7d684283ebc72d2fb92
isabellagh <isabellagh88@gmail.com>
2021-09-08 00:28:09 -0400

    rounded and centered picture

--- src/components/body/about/about.js
+++ src/components/body/about/about.js
@@ -4,8 +4,12 @@ import "./about.css";
 function about() {
   return (
     <div className="about">
-      <div className="about-picture">
-          <img src={require("../../../assets/profile.png").default} className="profile-picture" alt="" />
+      <div className="about-top">
+        <img
+          src={require("../../../assets/profile.png").default}
+          className="profile-picture"
+          alt=""
+        />
       </div>
       <div className="about-info">
         <p>
@@ -17,6 +21,9 @@ function about() {
           mini-projects, fixing the bugs I have created, and thinking about ways
           of helping people through technology.
         </p>
+      </div>
+      <div className="about-bottom">
+        <div className="contact"></div>
       </div>
     </div>
   );
