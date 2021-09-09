1c8cada756e6aa1faf4fa62cdc97ade544cadaf7
isabellagh <isabellagh88@gmail.com>
2021-09-08 22:14:36 -0400

    contact data renamed + contact and contsct css fil

---
+++ src/components/body/about/ContactData.js
@@ -0,0 +1,24 @@
+const ContactData = [
+    {
+        platform:"Github",
+        link:"https://github.com/isabellagh",
+        icon: require("../../../assets/icons/github (1).png").default,
+    },
+    {
+        platform:"Instagram",
+        link:"https://www.instagram.com/bella_ghenriques/",
+        icon: require("../../../assets/icons/instagram (3).png").default,
+    },
+    {
+        platform:"Twitter",
+        link:"https://twitter.com/Isagonsilveir",
+        icon: require("../../../assets/icons/twitter.png").default,
+    },
+    {
+        platform:"Linkedin",
+        link:"https://www.linkedin.com/in/isabella-henriques-4799662a/",
+        icon: require("../../../assets/icons/linkedin.png").default,
+    },
+]
+
+export default ContactData
--- src/components/body/about/contact.js
+++ src/components/body/about/contact.js
@@ -1,24 +1,14 @@
-const contact = [
-    {
-        platform:"Github",
-        link:"https://github.com/isabellagh",
-        icon: require("../../../assets/icons/github (1).png").default,
-    },
-    {
-        platform:"Instagram",
-        link:"https://www.instagram.com/bella_ghenriques/",
-        icon: require("../../../assets/icons/instagram (3).png").default,
-    },
-    {
-        platform:"Twitter",
-        link:"https://twitter.com/Isagonsilveir",
-        icon: require("../../../assets/icons/twitter.png").default,
-    },
-    {
-        platform:"Linkedin",
-        link:"https://www.linkedin.com/in/isabella-henriques-4799662a/",
-        icon: require("../../../assets/icons/linkedin.png").default,
-    },
-]
+import React from 'react'
+import { ContactData } from "./ContactData"
+import "./contact.css"
+
+function contact() {
+    const data = ContactData
+    return (
+        <div>
+            Contact data
+        </div>
+    )
+}
 
 export default contact
