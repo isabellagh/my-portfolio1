1a316003ffe053271b1869f5d632d6519cec6b3b
isabellagh <isabellagh88@gmail.com>
2021-09-08 22:22:01 -0400

    contact icons iteration

--- src/components/body/about/contact.js
+++ src/components/body/about/contact.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { ContactData } from "./ContactData"
-import "./contact.css"
+import React from "react";
+import { ContactData } from "./ContactData";
+import "./contact.css";
 
 function contact() {
-    const data = ContactData
-    return (
-        <div>
-            Contact data
-        </div>
-    )
+  const data = ContactData;
+  return (
+    <div className="contact-links">
+      {data.map((item) => {
+        return (
+          <a href={item.link}>
+            <div className="contact-icon-div">
+              <img src={item.icon} className="contact-icon" alt="" />
+            </div>
+          </a>
+        );
+      })}
+    </div>
+  );
 }
 
-export default contact
+export default contact;
