edf071c6be566c889f6461a4eeeadd279f6324ee
isabellagh <isabellagh88@gmail.com>
2021-09-08 22:47:08 -0400

    contact icons styled with media

--- src/components/body/about/ContactData.js
+++ src/components/body/about/ContactData.js
@@ -2,22 +2,22 @@ export const ContactData = [
     {
         platform:"Github",
         link:"https://github.com/isabellagh",
-        icon: require("../../../assets/icons/github (1).png").default,
+        icon: require("../../../assets/icons/github (5).png").default,
     },
     {
         platform:"Instagram",
         link:"https://www.instagram.com/bella_ghenriques/",
-        icon: require("../../../assets/icons/instagram (3).png").default,
+        icon: require("../../../assets/icons/instagram (4).png").default,
     },
     {
         platform:"Twitter",
         link:"https://twitter.com/Isagonsilveir",
-        icon: require("../../../assets/icons/twitter.png").default,
+        icon: require("../../../assets/icons/twitter (1).png").default,
     },
     {
         platform:"Linkedin",
         link:"https://www.linkedin.com/in/isabella-henriques-4799662a/",
-        icon: require("../../../assets/icons/linkedin.png").default,
+        icon: require("../../../assets/icons/linkedin (1).png").default,
     },
 ]
 
