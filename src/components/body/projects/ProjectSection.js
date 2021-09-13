b175c0e20d7ca98d616b3dc287e03d2707fe5bba
isabellagh <isabellagh88@gmail.com>
2021-09-12 22:28:42 -0400

    project about + centered info

--- src/components/body/projects/ProjectSection.js
+++ src/components/body/projects/ProjectSection.js
@@ -3,23 +3,23 @@ import "./project-section.css";
 
 function ProjectSection({ project }) {
   return (
-  <div className="project-section">
-    <div className="project-info">
-      <label className="project-title">{project.title}</label>
-      <div className="project-links">
-        {project.github &&
-          <a className="project-link" href={project.github}>
-            <div className="link-button">
-            <i class="devicon-github-original colored"></i> Github
-
-            </div>
-          </a>
-        }
-          
+    <div className="project-section">
+      <div className="project-info">
+        <label className="project-title">{project.title}</label>
+        <div className="project-about">{project.about}</div>
+        <div className="project-links">
+          {project.github && (
+            <a className="project-link" href={project.github}>
+              <div className="link-button">
+                <i class="devicon-github-original colored"></i> Github
+              </div>
+            </a>
+          )}
+        </div>
       </div>
-    </div>
       <img src={project.image} className="project-image" alt="" />
-  </div>
-)}
+    </div>
+  );
+}
 
 export default ProjectSection;
