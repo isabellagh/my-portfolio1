315055cef62e612c0473384eecb725303c37c40a
isabellagh <isabellagh88@gmail.com>
2021-09-14 21:38:17 -0400

    skills Data

--- src/components/body/skills/SkillsData.js
+++ src/components/body/skills/SkillsData.js
@@ -1,11 +1,37 @@
-import React from 'react'
+export const SkillsData = [
+  {
+    id: 1,
+    name: "Ruby",
+    icon: <i class="devicon-ruby-plain-wordmark colored"></i>,
+  },
 
-function SkillsData() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  {
+    id: 1,
+    name: "Ruby on Rails",
+    icon: <i class="devicon-rails-plain-wordmark colored"></i>,
+  },
 
-export default SkillsData
+  {
+    id: 1,
+    name: "JavaScript",
+    icon: <i class="devicon-javascript-plain colored"></i>,
+  },
+
+  {
+    id: 1,
+    name: "React",
+    icon: <i class="devicon-react-original-wordmark colored"></i>,
+  },
+
+  {
+    id: 1,
+    name: "SQL",
+    icon: <i class="devicon-postgresql-plain-wordmark colored"></i>,
+  },
+
+  {
+    id: 1,
+    name: "Node.js",
+    icon: <i class="devicon-nodejs-plain-wordmark colored"></i>,
+  },
+];
