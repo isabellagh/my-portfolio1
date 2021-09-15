b43b403f2aac2707eabe65f3c2d1ccab794e2234
isabellagh <isabellagh88@gmail.com>
2021-09-14 21:39:19 -0400

    skills data id fixed

--- src/components/body/skills/SkillsData.js
+++ src/components/body/skills/SkillsData.js
@@ -6,31 +6,31 @@ export const SkillsData = [
   },
 
   {
-    id: 1,
+    id: 2,
     name: "Ruby on Rails",
     icon: <i class="devicon-rails-plain-wordmark colored"></i>,
   },
 
   {
-    id: 1,
+    id: 3,
     name: "JavaScript",
     icon: <i class="devicon-javascript-plain colored"></i>,
   },
 
   {
-    id: 1,
+    id: 4,
     name: "React",
     icon: <i class="devicon-react-original-wordmark colored"></i>,
   },
 
   {
-    id: 1,
+    id: 5,
     name: "SQL",
     icon: <i class="devicon-postgresql-plain-wordmark colored"></i>,
   },
 
   {
-    id: 1,
+    id: 6,
     name: "Node.js",
     icon: <i class="devicon-nodejs-plain-wordmark colored"></i>,
   },
