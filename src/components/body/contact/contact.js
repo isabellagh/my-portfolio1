9f4e430c9544a4c99e3409e3dfee7f2f9dfb93a9
isabellagh <isabellagh88@gmail.com>
2021-09-21 21:55:26 -0400

    contact styling with resume download

--- src/components/body/contact/contact.js
+++ src/components/body/contact/contact.js
@@ -6,8 +6,17 @@ function contact() {
   return (
     <div className="contact-me">
       <h1>Contact</h1>
-      <div className="contact-media">
-        <Contact />
+      <div className="contact-container">
+        <div className="contact-left">
+          <p>You can contact me here:</p>
+          <Contact />
+        </div>
+        <div className="download">
+          <a download href={require('../../../assets/resume.pdf').default}>
+          <i class="bi bi-file-earmark-arrow-down"></i>
+            Download my Resume 
+          </a>
+        </div>
       </div>
     </div>
   );
