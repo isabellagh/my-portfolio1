8ba9d121cd6de32e3d1cd4b1e65f0de70cfa0f5a
isabellagh <isabellagh88@gmail.com>
2021-09-21 23:29:25 -0400

    ids added to sections

--- src/components/header/pc.js
+++ src/components/header/pc.js
@@ -5,22 +5,22 @@ import "./pc.css";
 function Pc() {
   return (
     <div className="pc">
-      <div className="pc-option">
+      <div className="pc-option" id="project">
         <Link to="/project">
           <i className="bi bi-clipboard-data option-icon">Projects </i>
         </Link>
       </div>
-      <div className="pc-option">
+      <div className="pc-option" id="skills">
         <Link to="/skills">
           <i className="bi bi-laptop option-icon">Skills </i>
         </Link>
       </div>
-      {/* <div className="pc-option">
+      {/* <div className="pc-option" id="work">
         <Link to="/work">
           <i className="bi bi-briefcase option-icon">Work </i>
         </Link>
       </div> */}
-      <div className="pc-option">
+      <div className="pc-option" id="contact">
         <Link to="/contact">
           <i className="bi bi-person option-icon">Contact</i>
         </Link>
