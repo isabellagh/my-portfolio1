2e9d91fa7d88dd3669b91448a25be2a74e8b6b35
isabellagh <isabellagh88@gmail.com>
2021-09-22 00:02:03 -0400

    navbar fixed top

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -9,19 +9,19 @@ import Contact from "./contact/contact";
 function Body() {
   return (
     <div className="body">
-    <section>
+    <section className="section">
       <About />
     </section>
-    <section>
+    <section className="section">
       <Projects />
     </section>
-    <section>
+    <section className="section">
       <Skills />
     </section>
-    {/* <section>
+    {/* <section className="section">
       <Experience />
     </section> */}
-    <section>
+    <section className="section">
       <Contact />
     </section>
     </div>
